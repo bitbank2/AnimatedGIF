6f5282b4ac1f40fd82ad5572bf9ccdcb225b1493
Laurence Bank <bitbank@pobox.com>
2020-07-26 13:25:35 -0400

    Added SD card example

---
+++ examples/adafruit_gfx_sdcard/adafruit_gfx_sdcard.ino
@@ -0,0 +1,179 @@
+#include <AnimatedGIF.h>
+
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+#include <SD.h>
+
+#define TFT_CS         10
+#define TFT_RST        8
+#define TFT_DC         9
+#define TFT_MOSI       11
+#define TFT_CLK        13
+#define TFT_MISO       12
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+AnimatedGIF gif;
+File f;
+
+void * GIFOpenFile(char *fname, int32_t *pSize)
+{
+  f = SD.open(fname);
+  if (f)
+  {
+    *pSize = f.size();
+    return (void *)&f;
+  }
+  return NULL;
+} /* GIFOpenFile() */
+
+void GIFCloseFile(void *pHandle)
+{
+  File *f = static_cast<File *>(pHandle);
+  if (f != NULL)
+     f->close();
+} /* GIFCloseFile() */
+
+int32_t GIFReadFile(GIFFILE *pFile, uint8_t *pBuf, int32_t iLen)
+{
+    int32_t iBytesRead;
+    iBytesRead = iLen;
+    File *f = static_cast<File *>(pFile->fHandle);
+    // Note: If you read a file all the way to the last byte, seek() stops working
+    if ((pFile->iSize - pFile->iPos) < iLen)
+       iBytesRead = pFile->iSize - pFile->iPos - 1; // <-- ugly work-around
+    if (iBytesRead <= 0)
+       return 0;
+    iBytesRead = (int32_t)f->read(pBuf, iBytesRead);
+    pFile->iPos = f->position();
+    return iBytesRead;
+} /* GIFReadFile() */
+
+int32_t GIFSeekFile(GIFFILE *pFile, int32_t iPosition)
+{ 
+  int i = micros();
+  File *f = static_cast<File *>(pFile->fHandle);
+  f->seek(iPosition);
+  pFile->iPos = (int32_t)f->position();
+  i = micros() - i;
+  Serial.printf("Seek time = %d us\n", i);
+  return pFile->iPos;
+} /* GIFSeekFile() */
+
+// Draw a line of image directly on the LCD
+void GIFDraw(GIFDRAW *pDraw)
+{
+    uint8_t *s;
+    uint16_t *d, *usPalette, usTemp[320];
+    int x, y;
+
+    usPalette = pDraw->pPalette;
+    y = pDraw->iY + pDraw->y; // current line
+    
+    s = pDraw->pPixels;
+    // Apply the new pixels to the main image
+    if (pDraw->ucHasTransparency) // if transparency used
+    {
+      uint8_t *pEnd, c, ucTransparent = pDraw->ucTransparent;
+      int x, iCount;
+      pEnd = s + pDraw->iWidth;
+      x = 0;
+      iCount = 0; // count non-transparent pixels
+      while(x < pDraw->iWidth)
+      {
+        c = ucTransparent-1;
+        d = usTemp;
+        while (c != ucTransparent && s < pEnd)
+        {
+          c = *s++;
+          if (c == ucTransparent) // done, stop
+          {
+            s--; // back up to treat it like transparent
+          }
+          else // opaque
+          {
+             *d++ = usPalette[c];
+             iCount++;
+          }
+        } // while looking for opaque pixels
+        if (iCount) // any opaque pixels?
+        {
+          tft.setAddrWindow(pDraw->iX+x, y, iCount, 1);
+          tft.startWrite();
+          tft.writePixels(usTemp, iCount, false, false);
+          tft.endWrite();
+          x += iCount;
+          iCount = 0;
+        }
+        // no, look for a run of transparent pixels
+        c = ucTransparent;
+        while (c == ucTransparent && s < pEnd)
+        {
+          c = *s++;
+          if (c == ucTransparent)
+             iCount++;
+          else
+             s--; 
+        }
+        if (iCount)
+        {
+          x += iCount; // skip these
+          iCount = 0;
+        }
+      }
+    }
+    else
+    {
+      s = pDraw->pPixels;
+      // Translate the 8-bit pixels through the RGB565 palette (already byte reversed)
+      for (x=0; x<pDraw->iWidth; x++)
+        usTemp[x] = usPalette[*s++];
+      tft.setAddrWindow(pDraw->iX, y, pDraw->iWidth, 1);
+      tft.startWrite();
+      tft.writePixels(usTemp, pDraw->iWidth, false, false);
+      tft.endWrite();
+    }
+} /* GIFDraw() */
+
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+// Note - some systems (ESP32?) require an SPI.begin() before calling SD.begin()
+// this code was tested on a Teensy 4.1 board
+
+  if(!SD.begin(BUILTIN_SDCARD))
+  {
+    Serial.println("SD Card mount failed!");
+    return;
+  }
+  else
+  {
+    Serial.println("SD Card mount succeeded!");
+  }
+
+  // put your setup code here, to run once:
+  tft.begin();
+  tft.fillScreen(ILI9341_BLACK);
+  gif.begin(LITTLE_ENDIAN_PIXELS);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("About to call gif.open");
+  if (gif.open((char *)"/GIF/HOMER.GIF", GIFOpenFile, GIFCloseFile, GIFReadFile, GIFSeekFile, GIFDraw))
+  {
+    Serial.printf("Successfully opened GIF; Canvas size = %d x %d\n", gif.getCanvasWidth(), gif.getCanvasHeight());
+    while (gif.playFrame(true, NULL))
+    {      
+    }
+    gif.close();
+  }
+  else
+  {
+    Serial.println("Error opening file");
+    while (1)
+    {};
+  }
+}
