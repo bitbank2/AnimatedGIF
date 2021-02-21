7dcc93f0645792866d439822783738843cc90ec6
Larry Bank <bitbank@pobox.com>
2021-02-21 17:58:14 -0500

    Update AnimatedGIF.h
    restored original buffer sizes

--- src/AnimatedGIF.h
+++ src/AnimatedGIF.h
@@ -34,10 +34,10 @@
 
 /* GIF Defines and variables */
 #define MAX_CHUNK_SIZE 255
-#define LZW_BUF_SIZE (12*MAX_CHUNK_SIZE)
-#define LZW_HIGHWATER (10*MAX_CHUNK_SIZE)
+#define LZW_BUF_SIZE (6*MAX_CHUNK_SIZE)
+#define LZW_HIGHWATER (4*MAX_CHUNK_SIZE)
 #define MAX_WIDTH 320
-#define FILE_BUF_SIZE 8192
+#define FILE_BUF_SIZE 4096
 
 #define PIXEL_FIRST 0
 #define PIXEL_LAST 4096
