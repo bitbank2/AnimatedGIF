e2f89865bfdf6fcc21039fddd3a17d106d2f06b0
Larry Bank <bitbank@pobox.com>
2025-06-18 00:56:37 +0100

    Added OneBitDisplay and non-ESP32 support

--- src/GIFPlayer.h
+++ src/GIFPlayer.h
@@ -20,22 +20,41 @@
 // limitations under the License.
 //===========================================================================
 #include "AnimatedGIF.h"
+// Supports 1-bit animations too
+#ifndef __ONEBITDISPLAY__
 #include <bb_spi_lcd.h>
+#endif
 #include <SD.h>
+#ifdef ARDUINO_ARCH_ESP32
 #include "FS.h"
 #include <LittleFS.h>
+#endif // ESP32
 
 // To ask the player to center the frame
 #define GIF_CENTER -2
+#ifdef __ONEBITDISPLAY__
+static ONE_BIT_DISPLAY *_pLCD;
+#else
 static BB_SPI_LCD *_pLCD;
+#endif
 static int _x, _y;
 
 class GIFPlayer
 {
   public:
+#ifdef __ONEBITDISPLAY__
+    int openData(ONE_BIT_DISPLAY *pLCD, const void *pData, int iDataSize);
+    int openSD(ONE_BIT_DISPLAY *pLCD, const char *fname);
+#ifdef ARDUINO_ARCH_ESP32
+    int openLFS(ONE_BIT_DISPLAY *pLCD, const char *fname);
+#endif // ESP32
+#else
     int openData(BB_SPI_LCD *pLCD, const void *pData, int iDataSize);
     int openSD(BB_SPI_LCD *pLCD, const char *fname);
+#ifdef ARDUINO_ARCH_ESP32
     int openLFS(BB_SPI_LCD *pLCD, const char *fname);
+#endif // ESP32
+#endif
     int getInfo(int *width, int *height);
     int play(int x, int y, bool bDelay);
     int close();
@@ -52,6 +71,7 @@ static void * gifOpenSD(const char *filename, int32_t *size) {
   }
   return NULL;
 }
+#ifdef ARDUINO_ARCH_ESP32
 static void * gifOpenLFS(const char *filename, int32_t *size) {
   static File myfile;
   myfile = LittleFS.open(filename, FILE_READ);
@@ -61,6 +81,8 @@ static void * gifOpenLFS(const char *filename, int32_t *size) {
   }
   return NULL;
 }
+#endif // ESP32
+
 static void gifClose(void *handle) {
   File *pFile = (File *)handle;
   if (pFile) pFile->close();
@@ -86,6 +108,73 @@ static int32_t gifSeek(GIFFILE *handle, int32_t iPosition) {
 }
 
 // Class implementation
+#ifdef __ONEBITDISPLAY__
+int GIFPlayer::openData(ONE_BIT_DISPLAY *pLCD, const void *pData, int iDataSize)
+{
+    int w, h, rc;
+    void *pBuf;
+    _gif.begin(GIF_PALETTE_1BPP_OLED);
+    if (_gif.open((uint8_t *)pData, iDataSize, NULL)) {
+        w = _gif.getCanvasWidth();
+        h =  _gif.getCanvasHeight();
+        pBuf = malloc((w * h) + ((w*h)/8));
+        if (!pBuf) {
+            return GIF_ERROR_MEMORY;
+        }
+        _gif.setFrameBuf(pBuf);
+        _gif.setDrawType(GIF_DRAW_COOKED);
+    } else {
+        return GIF_FILE_NOT_OPEN;
+    }
+    _pLCD = pLCD;
+    return GIF_SUCCESS;
+} /* openData() */
+
+int GIFPlayer::openSD(ONE_BIT_DISPLAY *pLCD, const char *fname)
+{
+    int w, h, rc;
+    void *pBuf;
+    _gif.begin(GIF_PALETTE_1BPP_OLED);
+    if (_gif.open(fname, gifOpenSD, gifClose, gifRead, gifSeek, NULL)) {
+        w = _gif.getCanvasWidth();
+        h =  _gif.getCanvasHeight();
+        pBuf = malloc((w * h) + ((w*h)/8));
+        if (!pBuf) {
+            _gif.close();
+            return GIF_ERROR_MEMORY;
+        }
+        _gif.setFrameBuf(pBuf);
+        _gif.setDrawType(GIF_DRAW_COOKED);
+    }
+    _pLCD = pLCD;
+    return GIF_SUCCESS;
+} /* openSD() */
+
+#ifdef ARDUINO_ARCH_ESP32
+int GIFPlayer::openLFS(ONE_BIT_DISPLAY *pLCD, const char *fname)
+{
+    int w, h, rc;
+    void *pBuf;
+    if (!LittleFS.begin(false)) {
+        return GIF_FILE_NOT_OPEN;
+    }
+    _gif.begin(GIF_PALETTE_1BPP_OLED);
+    if (_gif.open(fname, gifOpenLFS, gifClose, gifRead, gifSeek, NULL)) {
+        w = _gif.getCanvasWidth();
+        h =  _gif.getCanvasHeight();
+        pBuf = malloc((w * h) + ((w*h)/8));
+        if (!pBuf) {
+            _gif.close();
+            return GIF_ERROR_MEMORY;
+        }
+        _gif.setFrameBuf(pBuf);
+        _gif.setDrawType(GIF_DRAW_COOKED);
+    }
+    _pLCD = pLCD;
+    return GIF_SUCCESS;
+} /* openLFS() */
+#endif // ESP32
+#else
 int GIFPlayer::openData(BB_SPI_LCD *pLCD, const void *pData, int iDataSize)
 {
     int rc;
@@ -123,6 +212,7 @@ int GIFPlayer::openSD(BB_SPI_LCD *pLCD, const char *fname)
     return GIF_SUCCESS;
 } /* openSD() */
 
+#ifdef ARDUINO_ARCH_ESP32
 int GIFPlayer::openLFS(BB_SPI_LCD *pLCD, const char *fname)
 {
     int rc;
@@ -143,6 +233,8 @@ int GIFPlayer::openLFS(BB_SPI_LCD *pLCD, const char *fname)
     _pLCD = pLCD;
     return GIF_SUCCESS;
 } /* openLFS() */
+#endif // ESP32
+#endif // bb_spi_lcd vs OneBitDisplay
 
 int GIFPlayer::getInfo(int *width, int *height)
 {
@@ -159,7 +251,11 @@ int GIFPlayer::play(int x, int y, bool bDelay)
 {
     int rc, ty, cw, ch, w, h;
     int iX, iY;
+#ifdef __ONEBITDISPLAY__
+    uint8_t *d, *pPixels;
+#else
     uint16_t *pPixels;
+#endif
     if (x == GIF_CENTER) {
         x = (_pLCD->width() - _gif.getCanvasWidth())/2;
         if (x < 0) x = 0;
@@ -178,6 +274,17 @@ int GIFPlayer::play(int x, int y, bool bDelay)
     rc = _gif.playFrame(bDelay, NULL);
     if (!rc) _gif.reset(); // loop forever
     // Update the display with the new pixels
+#ifdef __ONEBITDISPLAY__
+    pPixels = (uint8_t *)(_gif.getFrameBuf() + (cw * ch));
+    d = (uint8_t *)_pLCD->getBuffer();
+    d += x + ((y/8) * _pLCD->width());
+    for (int ty=0; ty<h+7; ty+=8) {
+        memcpy(d, pPixels, cw);
+        d += _pLCD->width(); // columns = bytes per row
+        pPixels += cw; // source pitch = canvas width
+    }
+    _pLCD->display();
+#else
     _pLCD->setAddrWindow(_x + iX, _y + iY, w, h);
         pPixels = (uint16_t *)(_gif.getFrameBuf() + (cw * ch)); // cooked pixels start here
         pPixels += iX + (iY * cw);
@@ -187,6 +294,7 @@ int GIFPlayer::play(int x, int y, bool bDelay)
             _pLCD->pushPixels(pPixels, w);
             pPixels += cw; // canvas width to the next line
         } // for y
+#endif
     return rc;
 } /* play() */
 
